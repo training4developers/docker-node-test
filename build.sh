4155a4752058a8d7b3a60800a634316fe954f7cb
Eric W. Greene <eric@ericwgreene.com>
2016-03-14 22:06:26 -0500

    updated docker file

--- build.sh
+++
@@ -1,1 +0,0 @@
-docker build -t training4developers/node .
--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker run -i -t \
-	-p 49160:3000 \
-	-v `pwd`/app:/opt/app \
-	--entrypoint /bin/bash \
-	training4developers/node
